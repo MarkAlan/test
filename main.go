28814b6e5ecf4687ad1cf87349dce32d5088db89
Mark Pownell <Mark@Marks-MBP.carolina.rr.com>
2016-06-13 09:19:59 -0400

    First commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main(){
+
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/earth", handler2)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request){
+	var x = r.Referer()
+	fmt.Fprintf(w, "Hello World\n")
+	fmt.Fprintf(w, x)
+}
+
+func handler2(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hello Earth\n")
+}
